3b90c0229c247562bcb3453ef060fd78e80cb774
ankit-492 <chauhanankit492@gmial.com>
2024-08-28 17:25:55 +0530

    connected to mongoDB and intall dotenv

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
-import express from 'express';
-
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 
-app.listen(3000,()=>{
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("conected to mongoDB");
+}).catch((err)=>{
+console.log(err);
+});
+app.listen(3000, () => {
     console.log("server is listening on the port 3000");
 });
