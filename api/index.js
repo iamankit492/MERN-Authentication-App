8e7da3335570c587e4c916ad87951f73eace70c0
ankit-492 <chauhanankit492@gmial.com>
2024-08-28 19:38:39 +0530

    create api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
-import express from 'express'
-import mongoose from 'mongoose'
+import express from 'express';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js'
 dotenv.config();
 const app = express();
 
@@ -12,3 +13,5 @@ console.log(err);
 app.listen(3000, () => {
     console.log("server is listening on the port 3000");
 });
+
+app.use("/api/user",userRoutes);
